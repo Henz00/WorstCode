239aa405c46962a14372e20952c61aeec4fc43b9
Henrik <henha18@student.sdu.dk>
2020-02-14 23:20:57 +0100

    first commit

---
+++ Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Sum_Program
+{
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+            UInt16 length = 1 + 1;
+            UInt16 multiplier = 1 - 1 + 1 + 1 + 1;
+            UInt16 first = 1 - 1;
+            UInt16 second = 1 + 1 - 1;
+
+        displayInsertNumbers:
+            Console.Write("Insert integer a and b: 'a,b' (without '' symbol) >");
+        getNumbers:
+            string[] stringArray = Console.ReadLine().Split(',');
+
+            if (stringArray.Length != length)
+            {
+                Console.WriteLine("Not the correct amount in integers. Try again.");
+                goto displayInsertNumbers;
+            }
+
+            Int32[] numbers = new Int32[length];
+
+            for (Int32 i = multiplier; i < numbers.Length + multiplier; i += second)
+            {
+                numbers[i - multiplier] = Convert.ToInt32(stringArray[i - multiplier]);
+            }
+
+            if (numbers[first] == numbers[second])
+            {
+                Console.WriteLine("Sum: {0}", (numbers[first] + numbers[second]) * multiplier);
+            }
+            else
+            {
+                Console.WriteLine("Sum: {0}", numbers[first] + numbers[second]);
+            }
+
+            Console.Write(">");
+
+            goto getNumbers;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Sum_Program
+{
+    class Program
+    {
+        static void CleanCode(string[] args)
+        {
+            Console.WriteLine("Indtast tal a og b: 'a, b' (uden '' tegn) >");
+
+            while (true)
+            {
+                try
+                {
+                    String[] input = Console.ReadLine().Split(',');
+                    if (input.Length < 2)
+                    {
+                        Console.WriteLine("Try again.");
+                    }
+
+                    int a = 0;
+                    int b = 0;
+                    
+                    try
+                    {
+                        a = Convert.ToInt32(input[0]);
+                        b = Convert.ToInt32(input[1]);
+                    }
+                    catch (FormatException fe)
+                    {
+                        Console.WriteLine("This program only accepts whole numbers");
+                    }
+
+                    if (a == b)
+                    {
+                        Console.WriteLine("Sum: {0}", ((a + b) * 3));
+                    }
+                    else
+                    {
+                        Console.WriteLine("Sum: {0}", (a + b));
+                    }
+
+                    Console.Write(">");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("lol you failed");
+                }
+            }
+        }
+    }
+}
